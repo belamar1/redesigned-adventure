7cb2e5e1e7df21e746fe460cfab10e00a8d63e8e
Mera1 <42624675+belamar1@users.noreply.github.com>
2022-11-12 17:06:13 +0000

    Delete make_snippet_spec.rb

--- challenges copy/spec/make_snippet_spec.rb
+++
@@ -1,6 +0,0 @@
-require'make_snippet'
-RSpec.describe 'make_snippet' do
-    it 'returns a string with the first five words of the string'do
-      expect(make_snippet('My name is a big fun')).to eq('My name is a big...')
-end
-end
