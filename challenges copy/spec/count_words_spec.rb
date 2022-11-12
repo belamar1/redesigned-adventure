f4af0f8d8887bd42795b97a086933ee4f024b3f0
Mera1 <42624675+belamar1@users.noreply.github.com>
2022-11-12 17:06:04 +0000

    Delete count_words_spec.rb

--- challenges copy/spec/count_words_spec.rb
+++
@@ -1,7 +0,0 @@
-require'count_words'
-RSpec.describe "count_words" do
-    it "returns the number of words in a string" do
-        expect(count_words("Makers academy is the best")).to eq(5)
-    end
-    
-end
