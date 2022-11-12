9a2e550f7d29fc18198c2621f92a7d5095c48c1f
belamar1 <belamar75@gmail.com>
2022-11-12 07:01:01 +0000

    Typo

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -1,5 +1,5 @@
 require 'password_checker'
-describe PasswordChecker do
+RSpec.describe PasswordChecker do
     it "should check password length" do
         password_checker = PasswordChecker.new
         expect { password_checker.check("aBcDeFg") }.to raise_error("Invalid password, must be 8+ characters.")
--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,5 +1,5 @@
 require 'present'
-describe Present do
+RSpec.describe Present do
     it "should wrap an unwraped gifts" do
         present = Present.new
         present.wrap("a gift")
